0107a95eadde38b7a3168727f792953b18df8220
peter <pngugi.wainaina@gmail.com>
2023-08-06 21:36:51 +0300

    my object la submission

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+const employee = {
+    name: "Pitt",
+    streetAddress:"Kasarani,Nairobi"
+  }
+  
+  function updateEmployeeWithKeyAndValue(employee, name, streetAddress){
+    let newEmployee = {...employee};
+    newEmployee[name] = streetAddress;
+  
+    return newEmployee
+  
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, streetAddress){
+    employee[name] = streetAddress;
+  
+    return employee
+  }
+  function deleteFromEmployeeByKey(employee, name){
+    let newEmployee = {...employee};
+    delete newEmployee[name];
+  
+    return newEmployee
+  
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, name){
+    delete employee[name];
+  
+    return employee
+  }
+  
